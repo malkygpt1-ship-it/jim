d8bf3af2ab33040a8c5115ca81d9c38775eb287c
malkygpt1-ship-it <malkygpt1@gmail.com>
2026-08-17 22:33:12 +0100

    Add IBT decorative wood and soil supplier metadata

---
+++ supplier-data-decorative-wood-soil.js
@@ -0,0 +1,5 @@
+// Current IBT Decorative Wood and Soil supplier metadata for catalogue additions mat-597..mat-598.
+Object.assign(window.GF_SUPPLIER_DATA||(window.GF_SUPPLIER_DATA={}),{
+"mat-597":{supplier:"IBT Merchants",supplierSku:"AGGREGATES0130",supplierTitle:"Decorative Bark General Purpose 1M3 IBT Bulk Bag",supplierUrl:"https://www.ibtmerchants.co.uk/product/1250/decorative-wood-and-soil/5086/decorative-bark-general-purpose-1m3-ibt-bulk-bag",supplierImage:"https://www.ibtmerchants.co.uk/images/mediumimages/products/bark.jpg",supplierVerified:true},
+"mat-598":{supplier:"IBT Merchants",supplierSku:"AGGREGATES0325",supplierTitle:"Hardwood Play Wood Chip M3 IBT Bulk Bag",supplierUrl:"https://www.ibtmerchants.co.uk/product/1250/decorative-wood-and-soil/5112/hardwood-play-wood-chip-m3-ibt-bulk-bag",supplierImage:"https://www.ibtmerchants.co.uk/images/mediumimages/products/AGGREGATES0325.jpg",supplierVerified:true}
+});
