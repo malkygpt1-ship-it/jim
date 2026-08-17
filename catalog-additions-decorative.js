27c3766a07d0b3e29495916065d5643f320222aa
malkygpt1-ship-it <malkygpt1@gmail.com>
2026-08-17 22:12:22 +0100

    Add missing current IBT decorative chips to catalogue

---
+++ catalog-additions-decorative.js
@@ -0,0 +1,23 @@
+// Current IBT Decorative Chips products that did not exist in the legacy spreadsheet.
+// Verified against IBT product pages on 17 Aug 2026. Costs are current prices including VAT.
+window.GF_CATALOG_ADDITIONS_DECORATIVE=[
+{id:"mat-566",name:"Blossom Decorative Chips 20mm 25kg Bag",cost:9.71},
+{id:"mat-567",name:"Blossom Decorative Chips 20mm IBT Bulk Bag",cost:226.98},
+{id:"mat-568",name:"Custard Cream Mix IBT Bulk Bag",cost:198.00},
+{id:"mat-569",name:"Dolorock Mix 20mm IBT Bulk Bag",cost:198.00},
+{id:"mat-570",name:"Douglas Muir Quartz 20mm IBT Bulk Bag",cost:184.67},
+{id:"mat-571",name:"Douglas Muir Quartz Chip 20mm 25kg Bag",cost:9.65},
+{id:"mat-572",name:"Grey Granite 14mm IBT Bulk Bag",cost:116.70},
+{id:"mat-573",name:"Grey Granite 20mm 25kg Bag",cost:7.33},
+{id:"mat-574",name:"Grey Whin Chips 20mm IBT Bulk Bag",cost:113.32},
+{id:"mat-575",name:"Highland Sun Mix IBT Bulk Bag",cost:198.00},
+{id:"mat-576",name:"Ice Gold Mix IBT Bulk Bag",cost:198.00},
+{id:"mat-577",name:"Irish Dawn Mix IBT Bulk Bag",cost:198.00},
+{id:"mat-578",name:"Lanark Red Chips 20mm 25kg Bag",cost:10.20},
+{id:"mat-579",name:"Lanark Red Chips 20mm IBT Bulk Bag",cost:142.14},
+{id:"mat-580",name:"Moonstone Chips 20mm IBT Bulk Bag",cost:172.09},
+{id:"mat-581",name:"Moonstone Decorative Chip 20mm 25kg Bag",cost:8.00},
+{id:"mat-582",name:"Moray Diamond Mix IBT Bulk Bag",cost:198.00},
+{id:"mat-583",name:"Moreland Black Basalt Chip 20mm IBT Bulk Bag",cost:142.45},
+{id:"mat-584",name:"RED Granite Chip Correnie 20mm IBT Bulk Bag",cost:168.35}
+];
