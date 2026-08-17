6345d847fbb63af54278115c6a0f65735b6d39f8
malkygpt1-ship-it <malkygpt1@gmail.com>
2026-08-17 21:54:52 +0100

    Load latest matched material costs

--- data.js
+++ data.js
@@ -28,9 +28,9 @@ window.GF_DATA_PROMISE=Promise.all([
   gfFetchCatalogPart('catalog/materials-1.b64'),
   gfFetchCatalogPart('catalog/materials-2.b64'),
   gfFetchCatalogPart('catalog/tools.b64'),
-  gfLoadScript('supplier-costs.js')
+  gfLoadScript('supplier-costs.js').then(()=>gfLoadScript('supplier-costs-batch14.js'))
 ]).then(async([materials1,materials2,tools])=>({
-  catalogVersion:'2026-08-17-ibt-incvat-v2',
+  catalogVersion:'2026-08-17-ibt-incvat-v3',
   materials:gfApplySupplierCosts(await gfDecodeCatalog(materials1+materials2)),
   tools:await gfDecodeCatalog(tools)
 }));
